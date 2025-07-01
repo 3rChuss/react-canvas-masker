99a2633f62b885e7e4d55002fa50bb9b89f9df2c
Jesus Abril @ Codennecting <jabril@codennecting.com>
2025-07-01 18:07:00 +0200

    Add undo/redo functionality and update MaskEditorProps for canvasRef type safety

--- dist/src/maskEditor.d.ts
+++ dist/src/maskEditor.d.ts
@@ -1,13 +1,25 @@
 import * as React from "react";
 import "./maskEditor.less";
+export interface HistoryState {
+    imageData: ImageData;
+    timestamp: number;
+}
+export interface MaskEditorCanvasRef extends HTMLCanvasElement {
+    undo: () => void;
+    redo: () => void;
+    clear: () => void;
+}
 export interface MaskEditorProps {
     src: string;
-    canvasRef?: React.RefObject<HTMLCanvasElement>;
+    canvasRef?: React.RefObject<MaskEditorCanvasRef>;
     cursorSize?: number;
     onCursorSizeChange?: (size: number) => void;
     maskOpacity?: number;
     maskColor?: string;
     maskBlendMode?: "normal" | "multiply" | "screen" | "overlay" | "darken" | "lighten" | "color-dodge" | "color-burn" | "hard-light" | "soft-light" | "difference" | "exclusion" | "hue" | "saturation" | "color" | "luminosity";
+    onDrawingChange: (isDrawing: boolean) => void;
+    onUndoRequest?: () => void;
+    onRedoRequest?: () => void;
 }
 export declare const MaskEditorDefaults: {
     cursorSize: number;
